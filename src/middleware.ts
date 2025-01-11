751608f3bdc74a8512dfc329517919dbb07106ea
obi steve <92187790+skizzy51@users.noreply.github.com>
2025-01-11 17:42:28 +0100

    reverted next auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,3 @@
-// export { default } from "next-auth/middleware"
-
-import { NextRequestWithAuth, withAuth } from "next-auth/middleware"
-
-export default async function middleware(req: NextRequestWithAuth) {
-    // NextAuth middleware logic
-    const response = await withAuth(req)
-
-    return response
-}
+export { default } from "next-auth/middleware"
 
 export const config = { matcher: ["/dashboard", "/sell/:path*"] }
